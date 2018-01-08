184ff6cec77c6ba2949b9bea22ec95fe0297d576
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-08 11:02:16 +0000

    Create count32.c

---
+++ count32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,count=0;
+string s[1000];
+printf("enter a string");
+scanf("%s",s);
+for(i=0;s[i] != 0;i++)
+{
+ if(s[i]=="/0")
+ { 
+    count++;
+    
+    }
+    printf("total number of characters=%d,"count++);
+    }
+    }
